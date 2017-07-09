37013c797cb56e7a35472eea948243ac701ea81c
mww0595 <mww0595@gmail.com>
2017-07-09 15:56:21 +0800

    Talking to other vc with subjects

--- 04-observables-in-practice/starter/Combinestagram/MainViewController.swift
+++ 04-observables-in-practice/starter/Combinestagram/MainViewController.swift
@@ -29,25 +29,71 @@ class MainViewController: UIViewController {
   @IBOutlet weak var buttonClear: UIButton!
   @IBOutlet weak var buttonSave: UIButton!
   @IBOutlet weak var itemAdd: UIBarButtonItem!
+    
+    private let bag = DisposeBag()
+    private let images = Variable<[UIImage]>([])
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    images.asObservable().subscribe(onNext: { [weak self] photos in
+        guard let imagePreview = self?.imagePreview else{
+            return
+        }
+        imagePreview.image = UIImage.collage(images: photos, size: imagePreview.frame.size)
+    })
+    .addDisposableTo(bag)
+    
+    images.asObservable().subscribe(onNext: { [weak self] photos in
+        self?.updateUI(photos: photos)
+    })
+    .addDisposableTo(bag)
   }
 
+    
+    
+    private func updateUI(photos: [UIImage]) {
+        buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
+        buttonClear.isEnabled = photos.count > 0
+        itemAdd.isEnabled = photos.count < 6
+        title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
+    }
+    
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
   }
 
   @IBAction func actionClear() {
+    images.value = []
 
   }
 
   @IBAction func actionSave() {
+    guard let image = imagePreview.image else {
+        return
+    }
+    PhotoWriter.save(image).subscribe( onError: {[weak self] error in
+        self?.showMessage("error", description: error.localizedDescription)
+    }, onCompleted: {[weak self] error in
+        self?.showMessage("saved")
+        self?.actionClear()
+    })
+    .addDisposableTo(bag)
   }
 
   @IBAction func actionAdd() {
-
+    let photosViewController = storyboard!.instantiateViewController(
+        withIdentifier: "PhotosViewController") as! PhotosViewController
+    navigationController!.pushViewController(photosViewController, animated:
+        true)
+    photosViewController.selectedPhotos.subscribe(onNext: {[weak self] newImage in
+        guard let images = self?.images else{
+            return
+        }
+        images.value.append(newImage)
+        }, onDisposed: {
+            print("completed photo selection")
+    }   )
+        .addDisposableTo(bag)
   }
 
   func showMessage(_ title: String, description: String? = nil) {
--- 04-observables-in-practice/starter/Combinestagram/PhotosViewController.swift
+++ 04-observables-in-practice/starter/Combinestagram/PhotosViewController.swift
@@ -22,13 +22,17 @@
 
 import UIKit
 import Photos
+import RxSwift
 
 class PhotosViewController: UICollectionViewController {
 
   // MARK: public properties
-
+    var selectedPhotos: Observable<UIImage> {
+        return selectedPhotosSubject.asObservable()
+    }
   // MARK: private properties
-
+    fileprivate let selectedPhotosSubject = PublishSubject<UIImage> ()
+    
   private lazy var photos = PhotosViewController.loadPhotos()
   private lazy var imageManager = PHCachingImageManager()
 
@@ -52,7 +56,7 @@ class PhotosViewController: UICollectionViewController {
 
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
-    
+//    selectedPhotosSubject.onCompleted()
   }
 
   // MARK: UICollectionView
@@ -85,7 +89,9 @@ class PhotosViewController: UICollectionViewController {
 
     imageManager.requestImage(for: asset, targetSize: view.frame.size, contentMode: .aspectFill, options: nil, resultHandler: { [weak self] image, info in
       guard let image = image, let info = info else { return }
-
+        if let isThumbnail = info[PHImageResultIsDegradedKey as NSString] as? Bool, !isThumbnail {
+            self?.selectedPhotosSubject.onNext(image)
+        }
 
     })
   }
